a70dc1905c35f87f61ca2273f59932801ce5f00d
mannewdan <109920968+mannewdan@users.noreply.github.com>
2023-03-01 01:55:48 -0600

    image fix

--- src/component/Subscore.tsx
+++ src/component/Subscore.tsx
@@ -5,12 +5,10 @@ type SubscoreProps = {
 };
 
 export default function Subscore({ data }: SubscoreProps) {
-  const imgUrl = new URL("./img.png", import.meta.url).href;
-
   return (
     <div className={`subscore ${data.category}`}>
       <div>
-        <img src={"." + data.icon}></img>
+        <img src={data.icon}></img>
         <span>{data.category}</span>
       </div>
       <div>
